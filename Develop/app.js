e5b30f92a823759cf87ea617480ca14c0f526e3d
Kay0s <hamilton.kristina@gmail.com>
2020-12-10 07:45:55 -0600

    fixed syntax for if, if else, else role question

--- Develop/app.js
+++ Develop/app.js
@@ -20,76 +20,80 @@ const render = require("./lib/htmlRenderer");
 //Intern?s : name, role, ID, email, and School
 // array of questions for user
 
-const team = [];
-
-function generateTeam() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the team member's name?",
-        validate: answer => {
-          if (answer === ""){
-            return true;
-          }
-          return "Please enter at least one character.";
-        }
-      },
-    
-      {
-        type: "input",
-        name: "id",
-        message: "What the team member's id?",
-        validate: answer => {
-      if (answer === 'number' ) {
-        return true;
-      }
-      return "Team member id must include a number.";
-    }
-  },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the team member's email",
-        validate: answer => {
-      if( /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/){
-        return true;
-        }
-        return "Please enter a valid email address";
-      }
-      },
-      {
-        type: "list",
-        name: "role",
-        message: "What is team member's role?",
-        choices: ["Manager", "Engineer", "Intern"],
-        validate: answer => {
-          if (answer === ""){
-            return true;
-          }
-          return "Please choose team member's role.";
-        }
-      },
-    ])  
-    .then((answer, choices) => {
-    if (choices === "Manager"){
-    inquirer.prompt([  
-      {
-        message:"What is the office number?",
-        name: "officeNumber",
-      },
-    } else if (choices === "Engineer"){
-      inquirer.prompt([  
+const team = [
+  function generateTeam() {
+    inquirer
+      .prompt([
         {
-          message:"What is the GitHub Username?",
-          name: "github",
-        },
-      } else (choices === "Intern") {
-        inquirer.prompt([  
-          {
-            message:"What is the school name?",
-            name: "school",
+          type: "input",
+          name: "name",
+          message: "What is the team member's name?",
+          validate: (answer) => {
+            if (answer === "") {
+              return true;
+            }
+            return "Please enter at least one character.";
           },
-        })
-        
+        },
+
+        {
+          type: "input",
+          name: "id",
+          message: "What the team member's id?",
+          validate: (answer) => {
+            if (answer === "number") {
+              return true;
+            }
+            return "Team member id must include a number.";
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the team member's email",
+          validate: (answer) => {
+            if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) {
+              return true;
+            }
+            return "Please enter a valid email address";
+          },
+        },
+        {
+          type: "list",
+          name: "role",
+          message: "What is team member's role?",
+          choices: ["Manager", "Engineer", "Intern"],
+          validate: (answer) => {
+            if (answer === "") {
+              return true;
+            }
+            return "Please choose team member's role.";
+          },
+        },
+      ])
+      .then((answer, choices) => {
+        if (choices === "Manager") {
+          inquirer.prompt([
+            {
+              message: "What is the office number?",
+              name: "officeNumber",
+            },
+          ]);
+        } else if (choices === "Engineer") {
+          inquirer.prompt([
+            {
+              message: "What is the GitHub Username?",
+              name: "github",
+            },
+          ]);
+        } else {
+          inquirer.prompt([
+            {
+              message: "What is the school name?",
+              name: "school",
+            },
+          ]);
+        }
+      });
+  },
+];
